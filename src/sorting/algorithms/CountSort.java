a63458a2ca53297e41e3f6c6b24e505818276005
DevDaniloFerrari <daniloferrari2000@hotmail.com>
2019-09-29 12:39:17 -0300

    Implementando CountSort

--- src/sorting/algorithms/CountSort.java
+++ src/sorting/algorithms/CountSort.java
@@ -1,4 +1,41 @@
 package sorting.algorithms;
 
-public class CountSort {
+import sorting.interfaces.Sort;
+
+public class CountSort implements Sort {
+    @Override
+    public int[] ordenar(int[] elementos) {
+        int tamanhoDoVetorPrincipal = elementos.length;
+        int maiorElementoDoVetorPrincipal=elementos[0];
+
+        for(int i=1; i < tamanhoDoVetorPrincipal-1; i++){
+            if(elementos[i]>maiorElementoDoVetorPrincipal)
+                maiorElementoDoVetorPrincipal = elementos[i];
+        }
+
+        int tamanhoDoVetorAuxiliar = maiorElementoDoVetorPrincipal;
+
+        int[] vetorAuxiliar = new int[tamanhoDoVetorAuxiliar+1];
+
+        for(int i=0; i < tamanhoDoVetorPrincipal; i++){
+            vetorAuxiliar[elementos[i]]++;
+        }
+
+        for(int i=1; i < tamanhoDoVetorAuxiliar+1; i++){
+            vetorAuxiliar[i] = vetorAuxiliar[i-1] + vetorAuxiliar[i];
+        }
+
+        int[] vetorOrdenado = new int[tamanhoDoVetorPrincipal+1];
+
+        for(int i=1; i < tamanhoDoVetorPrincipal+1; i++){
+            vetorOrdenado[vetorAuxiliar[elementos[i-1]]] = elementos[i-1];
+            vetorAuxiliar[elementos[i-1]]++;
+        }
+
+        for(int i=0; i < tamanhoDoVetorPrincipal; i++){
+            elementos[i] = vetorOrdenado[i+1];
+        }
+
+        return elementos;
+    }
 }
--- src/sorting/tests/SortTests.java
+++ src/sorting/tests/SortTests.java
@@ -4,24 +4,27 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.testng.Assert;
 import sorting.algorithms.BubbleSort;
+import sorting.algorithms.CountSort;
 import sorting.algorithms.HeapSort;
 import sorting.algorithms.InsertionSort;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class SortTests {
 
-    int[] elementosOrdenados = {1,2,3,4,5,6,7,8,9,10};
-    int[] elementosDesordenados = {3,9,4,1,10,8,7,5,6,2};
+    int[] elementosOrdenados = {0,1,2,3,4,5,6,7,8,9,10};
+    int[] elementosDesordenados = {3,9,4,0,1,10,8,7,5,6,2};
 
     BubbleSort bubbleSort;
     InsertionSort insertionSort;
     HeapSort heapSort;
+    CountSort countSort;
 
     @BeforeAll
     public void inicializarTest(){
         bubbleSort = new BubbleSort();
         insertionSort = new InsertionSort();
         heapSort = new HeapSort();
+        countSort = new CountSort();
     }
 
     @Test
@@ -37,5 +40,10 @@ public class SortTests {
     @Test
     public void heapSortTest(){
         Assert.assertEquals(elementosOrdenados, heapSort.ordenar(elementosDesordenados));
+    }
+
+    @Test
+    public void countSortTest(){
+        Assert.assertEquals(elementosOrdenados, countSort.ordenar(elementosDesordenados));
     }
 }
