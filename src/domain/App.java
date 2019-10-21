7ac434bac00573abbeb3b24f15f13c59a6ad5e7c
Manoel <manoelmerlin@hotmail.com>
2019-10-21 10:33:57 -0300

    Implementação bucket sort

--- src/domain/App.java
+++ src/domain/App.java
@@ -1,12 +1,19 @@
 package domain;
 
+import sorting.algorithms.BucketSort;
 import sorting.algorithms.MergeSort;
 import sorting.algorithms.QuickSort;
 
 import java.util.Arrays;
 
+
 public class App {
     public static void main(String[] args) {
 
+        BucketSort bucketSort = new BucketSort();
+
+        int[] elementosDesordenados = {3,9,4,0,1,10,8,7,5,6,2};
+        bucketSort.ordenar(elementosDesordenados);
+        System.out.println(Arrays.toString(elementosDesordenados));
     }
 }
--- src/sorting/algorithms/BucketSort.java
+++ src/sorting/algorithms/BucketSort.java
@@ -1,4 +1,45 @@
 package sorting.algorithms;
 
-public class BucketSort {
+import sorting.interfaces.Sort;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+public class BucketSort implements Sort {
+
+    @Override
+    public int[] ordenar(int[] elementos) {
+
+        int maiorNumeroVetor = 0;
+
+        for(int i = 0; i < elementos.length; i++) {
+            if (elementos[i] > maiorNumeroVetor) {
+                maiorNumeroVetor = elementos[i];
+            }
+        }
+
+        List<Integer>[] baldes = new List[maiorNumeroVetor];
+        for(int i = 0; i < maiorNumeroVetor; i++){
+            baldes[i] = new LinkedList<>();
+        }
+
+        for(int numeroOrdenado : elementos){
+            baldes[hash(numeroOrdenado)].add(numeroOrdenado);
+        }
+        for(List<Integer> balde : baldes){
+            Collections.sort(balde);
+        }
+        int i = 0;
+        for(List<Integer> balde : baldes){
+            for(int numeroOrdenado : balde){
+                elementos[i++] = numeroOrdenado;
+            }
+        }
+        return elementos;
+    }
+
+    private static int hash(int num){
+        return num/10;
+    }
 }
